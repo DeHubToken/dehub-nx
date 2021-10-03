ead0102535c5c285004335aab14ebf2ee8bff7d1
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2021-10-03 15:53:24 +0200

    lottery: add a change which not break tests

--- apps/lottery/src/app/app.tsx
+++ apps/lottery/src/app/app.tsx
@@ -7,7 +7,7 @@ export function App() {
     <div className={styles.app}>
       <header className="flex">
         <Logo width="75" height="75" />
-        <h1>Welcome to lottery Dapp!</h1>
+        <h1>Welcome to lottery!</h1>
       </header>
       <main>
         <h2>Resources &amp; Tools</h2>
