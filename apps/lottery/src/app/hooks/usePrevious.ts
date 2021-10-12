ee1bb478a4bde8b47b6e61dc6ffb17288c6c6bae
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2021-10-12 19:55:17 +0200

    eslint: multiline comment style

--- apps/lottery/src/app/hooks/usePrevious.ts
+++ apps/lottery/src/app/hooks/usePrevious.ts
@@ -2,8 +2,10 @@ import { useEffect, useRef } from 'react'
 
 // modified from https://usehooks.com/usePrevious/
 export default function usePrevious<T>(value: T) {
-  // The ref object is a generic container whose current property is mutable ...
-  // ... and can hold any value, similar to an instance property on a class
+  /*
+   * The ref object is a generic container whose current property is mutable ...
+   * ... and can hold any value, similar to an instance property on a class
+   */
   const ref = useRef<T>()
 
   // Store current value in ref
