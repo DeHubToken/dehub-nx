f295301710f9d29838ee8a146bbf42ff69e10aa5
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2021-10-15 16:47:46 +0200

    landing: top content

--- apps/web/src/app/view/landing.component.ts
+++ apps/web/src/app/view/landing.component.ts
@@ -1,7 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  template: `<h1>Landing</h1>`,
+  template: `
+    <div class="grid">
+      <div class="col-12">
+        <div class="text-center">
+          <img
+            src="assets/dehub/icon-dehub-white.svg"
+            alt="Dehub Icon"
+            width="90px"
+            class="m-4"
+          />
+        </div>
+      </div>
+
+      <div class="col-4">
+        <div class="text-center">DECENTRALIZED</div>
+      </div>
+
+      <div class="col-4">
+        <div class="text-center">ENTERTAINMENT</div>
+      </div>
+
+      <div class="col-4">
+        <div class="text-center">HUB</div>
+      </div>
+    </div>
+  `,
+  styles: [``],
 })
 export class LandingComponent implements OnInit {
   ngOnInit() {}
