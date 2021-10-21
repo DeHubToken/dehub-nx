bdf9597685aa957349a24f3a72856567a093f684
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2021-10-21 21:00:29 +0200

    landing: custom dehub landing scss

--- apps/web/src/app/view/landing/landing-trikenomic/landing-trikenomic.component.ts
+++ apps/web/src/app/view/landing/landing-trikenomic/landing-trikenomic.component.ts
@@ -1,21 +1,36 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+export interface Trikenomic {
+  icon: string[];
+  percent?: number;
+  title: string;
+  subTitle: string;
+}
 @Component({
   selector: 'dhb-landing-trikenomic',
   template: `
-    <p>
-      landing-trikenomic works!
-    </p>
+    <div *ngIf="trikenomic" class="pricing-card enterprise shadow-8">
+      <span class="time"><i [ngClass]="trikenomic.icon"></i></span>
+      <h2>{{ trikenomic.title }}</h2>
+      <span *ngIf="trikenomic.percent" class="price"
+        >{{ trikenomic.percent }}%</span
+      >
+      <ul>
+        <li>{{ trikenomic.subTitle }}</li>
+      </ul>
+    </div>
   `,
-  styles: [
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LandingTrikenomicComponent implements OnInit {
+  @Input() trikenomic?: Trikenomic;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
