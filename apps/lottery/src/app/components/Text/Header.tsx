122d28e22d78f755afdc32710902314b2bcf1a91
Aleksandr Pasevin <pasevin@gmail.com>
2021-10-24 04:09:15 +0300

    style: fontWeight param for Header component

--- apps/lottery/src/app/components/Text/Header.tsx
+++ apps/lottery/src/app/components/Text/Header.tsx
@@ -3,9 +3,11 @@ import styled from 'styled-components';
 export const Header = styled.div<{
   color?: string;
   fontSize?: string;
+  fontWeight?: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
 }>`
   color: ${({ color }) => color ?? 'inherit'};
   font-size: ${({ fontSize }) => fontSize ?? '20px'};
+  font-weight: ${({ fontWeight }) => fontWeight ?? '600'};
 `;
 
 export const Title = styled.div<{
