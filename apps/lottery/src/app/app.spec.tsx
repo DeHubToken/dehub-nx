21e4bf8f766d5c29f1aae6c25406b8767df22a11
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2021-10-25 18:13:27 +0200

    lottery: test - fix Warning: An update to MoralisProvider inside a test was not wrapped in act(...).

--- apps/lottery/src/app/app.spec.tsx
+++ apps/lottery/src/app/app.spec.tsx
@@ -1,11 +1,14 @@
-import { render } from '@testing-library/react';
-
+import { act, render } from '@testing-library/react';
 import App from './app';
 
 describe('App', () => {
-  it('should render successfully', () => {
+  it('should render successfully', async () => {
+    const promise = Promise.resolve();
     const { baseElement } = render(<App />);
 
     expect(baseElement).toBeTruthy();
+
+    // Why act needed: https://kentcdodds.com/blog/fix-the-not-wrapped-in-act-warning
+    await act(() => promise);
   });
 });
