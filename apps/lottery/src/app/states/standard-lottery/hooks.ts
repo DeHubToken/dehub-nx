d7f09f3e299911ed3a66e6e3626d81f4e8c77c7e
deep-quality-dev <hungrywarrior081@gmail.com>
2021-10-28 03:06:04 +0800

    fix: remove unused import

--- apps/lottery/src/app/states/standard-lottery/hooks.ts
+++ apps/lottery/src/app/states/standard-lottery/hooks.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import BigNumber from 'bignumber.js';
 import { useSelector } from 'react-redux';
 
