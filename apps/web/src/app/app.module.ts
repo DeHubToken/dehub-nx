f9eac4cbe824caeed16e15eac731013e7aebf6d9
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2021-10-27 16:18:03 +0200

    web: turn on PWA (#36)

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { MenuModule } from 'primeng/menu';
 import { RadioButtonModule } from 'primeng/radiobutton';
 import { TabViewModule } from 'primeng/tabview';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppMainComponent } from './app.main.component';
@@ -17,6 +19,7 @@ import { AppMenuComponent } from './menu/app.menu.component';
 import { MenuService } from './menu/app.menu.service';
 import { AppMenuitemComponent } from './menu/app.menuitem.component';
 import { AppTopBarComponent } from './topbar/app.topbar.component';
+
 @NgModule({
   imports: [
     CommonModule,
@@ -30,6 +33,14 @@ import { AppTopBarComponent } from './topbar/app.topbar.component';
     MenuModule,
     RadioButtonModule,
     TabViewModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      /**
+       * Register the ServiceWorker as soon as the app is stable
+       * or after 30 seconds (whichever comes first)
+       */
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   declarations: [
     AppComponent,
