e0c2e946397eb201ec6effa40d9009299a2f9fcb
Ben Weider <84747155+deep-quality-dev@users.noreply.github.com>
2021-11-01 04:41:31 +0800

    env(lottery): mainnet environment(temporary) (#70)

--- apps/lottery/src/environments/environment.ts
+++ apps/lottery/src/environments/environment.ts
@@ -7,7 +7,7 @@ import { ChainId } from '@dehub/shared/config';
 
 export const environment = {
   production: false,
-  chainId: ChainId.BSC_TESTNET,
+  chainId: ChainId.BSC_MAINNET,
   deGrandStartDay: 26,
   deGrandStartDayOnFebruary: 25,
 };
