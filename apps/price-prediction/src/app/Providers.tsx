0438e0ab9ee086913f5d85b3cdbe3442fcc35441
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2021-11-16 16:03:38 +0100

    fix: app casing, proper imports, remove custom webpack config

--- apps/price-prediction/src/app/Providers.tsx
+++ apps/price-prediction/src/app/Providers.tsx
@@ -5,12 +5,12 @@ import { ModalProvider } from '@pancakeswap/uikit';
 import { Web3ReactProvider } from '@web3-react/core';
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider } from 'react-redux';
-import { getLibrary } from 'utils/web3React';
-import { ThemeContextProvider } from 'contexts/ThemeContext';
-import { LanguageProvider } from 'contexts/Localization';
-import { RefreshContextProvider } from 'contexts/RefreshContext';
-import { ToastsProvider } from 'contexts/ToastsContext';
-import store from 'state';
+import { getLibrary } from './utils/web3React';
+import { ThemeContextProvider } from './contexts/ThemeContext';
+import { LanguageProvider } from './contexts/Localization';
+import { RefreshContextProvider } from './contexts/RefreshContext';
+import { ToastsProvider } from './contexts/ToastsContext';
+import store from './state';
 
 const Providers: React.FC = ({ children }) => {
   return (
