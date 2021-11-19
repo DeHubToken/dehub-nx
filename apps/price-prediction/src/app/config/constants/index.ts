a9f82f5846eafef105c49de62d68c6f3f674c584
Ben Weider <ben.weider081@yahoo.com>
2021-11-19 22:18:48 +0800

    feat: connect wallet loader

--- apps/price-prediction/src/app/config/constants/index.ts
+++ apps/price-prediction/src/app/config/constants/index.ts
@@ -1,6 +1,10 @@
-import { ChainId } from '@dehub/shared/config';
+import { ChainId, Constants } from '@dehub/shared/config';
 import { environment } from '../../../environments/environment';
 
 export const getChainId = (): ChainId => {
   return environment.chainId;
+};
+
+export const getChainIdHex = (): string => {
+  return Constants[getChainId()].CHAIN_ID_HEX;
 };
