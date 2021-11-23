f6ce6b6951cc42ae1508ea0db0a63dcb41f5a1d4
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2021-11-23 16:59:27 +0100

    lottery: fix test, proper import resolution with moduleNameMapper in jest

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,12 @@
 const nxPreset = require('@nrwl/jest/preset');
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  moduleNameMapper: {
+    '^lodash-es$': 'lodash',
+    '^@dehub/shared/assets/dehub/(.*)$':
+      '<rootDir>/../../libs/shared/assets/dehub/src/lib/$1',
+    '^@dehub/shared/assets/freya/(.*)$':
+      '<rootDir>/../../libs/shared/assets/freya/src/lib/$1',
+  },
+};
