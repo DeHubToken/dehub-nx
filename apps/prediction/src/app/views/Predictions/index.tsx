c0d578328e23cc9d0a945bb41b75b8f1d35c983d
Ben Weider <ben.weider081@yahoo.com>
2021-11-25 21:56:48 +0800

    re-style(prediction): background image

--- apps/prediction/src/app/views/Predictions/index.tsx
+++ apps/prediction/src/app/views/Predictions/index.tsx
@@ -215,7 +215,7 @@ const Predictions = () => {
             className="layout-wrapper"
             style={{
               background:
-                'linear-gradient(45deg, rgba(11, 17, 19, 0.95), rgba(5, 17, 24, 0.9) 46%, rgba(6, 12, 29, 0.8) 71%, rgba(50, 19, 56, 0.95)), url("assets/img/prize-draw-bg.jpg") no-repeat fixed center center /cover',
+                'linear-gradient(45deg, rgba(11, 17, 19, 0.95), rgba(5, 17, 24, 0.9) 46%, rgba(6, 12, 29, 0.8) 71%, rgba(50, 19, 56, 0.95)), url("assets/img/prediction-bg.jpg") no-repeat fixed center center /cover',
             }}
           >
             <Header
