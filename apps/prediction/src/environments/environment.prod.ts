35187d4196bc921ed2030782ca09f3dc3e155314
Ben Weider <ben.weider081@yahoo.com>
2021-11-25 18:37:42 +0800

    fix(prediction): subdomain configuration, css import by using helmet

--- apps/prediction/src/environments/environment.prod.ts
+++ apps/prediction/src/environments/environment.prod.ts
@@ -3,6 +3,5 @@ import { ChainId } from '@dehub/shared/config';
 export const environment = {
   production: true,
   chainId: ChainId.BSC_MAINNET,
-  deGrandStartDay: 26,
-  deGrandStartDayOnFebruary: 25,
+  baseUrl: '/prediction',
 };
--- apps/prediction/src/environments/environment.ts
+++ apps/prediction/src/environments/environment.ts
@@ -8,6 +8,5 @@ import { ChainId } from '@dehub/shared/config';
 export const environment = {
   production: false,
   chainId: ChainId.BSC_TESTNET,
-  deGrandStartDay: 26,
-  deGrandStartDayOnFebruary: 25,
+  baseUrl: '',
 };
