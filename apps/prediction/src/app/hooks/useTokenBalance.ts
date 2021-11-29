92a1d0624ba40b13856c3ac5e00179e1badaffae
fazlerabi <fazle.rabbi.dev@gmail.com>
2021-11-29 09:45:07 -0500

    fix(prediction): change network

--- apps/prediction/src/app/hooks/useTokenBalance.ts
+++ apps/prediction/src/app/hooks/useTokenBalance.ts
@@ -6,12 +6,12 @@ import { BIG_ZERO } from '../utils/bigNumber';
 
 const useTokenBalance = (tokenAddress: string) => {
   const [balance, setBalance] = useState(BIG_ZERO);
-  const { account, signer } = Hooks.useMoralisEthers();
+  const { account } = Hooks.useMoralisEthers();
   const { fastRefresh } = Hooks.useRefresh();
 
   useEffect(() => {
     const fetchBalance = async () => {
-      const contract = getBep20Contract(tokenAddress, signer);
+      const contract = getBep20Contract(tokenAddress);
       const res = await contract.balanceOf(account);
       setBalance(new BigNumber(res.toString()));
     };
@@ -19,7 +19,7 @@ const useTokenBalance = (tokenAddress: string) => {
     if (account) {
       fetchBalance();
     }
-  }, [account, tokenAddress, signer, fastRefresh]);
+  }, [account, tokenAddress, fastRefresh]);
 
   return balance;
 };
