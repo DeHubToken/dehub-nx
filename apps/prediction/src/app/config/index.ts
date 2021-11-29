f88a52ed38a6f976f9e71b0eb3f85cf694e39ac2
sbvhev <rohannepal200@gmail.com>
2021-11-29 12:23:13 -0500

    feat: update decimal point

--- apps/prediction/src/app/config/index.ts
+++ apps/prediction/src/app/config/index.ts
@@ -30,5 +30,5 @@ export const BASE_LIQUIDITY_POOL_URL = `${BASE_EXCHANGE_URL}/#/pool`;
 export const BASE_BSC_SCAN_URL = 'https://bscscan.com';
 export const LOTTERY_MAX_NUMBER_OF_TICKETS = 50;
 export const LOTTERY_TICKET_PRICE = 1;
-export const DEFAULT_TOKEN_DECIMAL = BIG_TEN.pow(18);
+export const DEFAULT_TOKEN_DECIMAL = BIG_TEN.pow(5);
 export const DEFAULT_GAS = 200000;
