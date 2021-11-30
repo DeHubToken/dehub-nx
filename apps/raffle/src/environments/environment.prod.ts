9b3bda43b50d39ea33d0516946aa54f2d9627cf2
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2021-11-30 14:12:08 +0100

    feat(single-domain): npm script deploy support (xxx:deploy:draft, xxx:deploy:prod), prod env and local demo fix

--- apps/raffle/src/environments/environment.prod.ts
+++ apps/raffle/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ import { ChainId, defaultSharedProdEnv } from '@dehub/shared/config';
 import { Env, defaultEnv } from './env';
 
 export const environment: Env = {
-  ...defaultSharedProdEnv,
   ...defaultEnv,
+  ...defaultSharedProdEnv,
 
   baseUrl: '/raffle',
 
--- apps/web/src/environments/environment.prod.ts
+++ apps/web/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ import { defaultSharedProdEnv } from '@dehub/shared/config';
 import { defaultEnv, Env } from './env';
 
 export const environment: Env = {
-  ...defaultSharedProdEnv,
   ...defaultEnv,
+  ...defaultSharedProdEnv,
 
   baseUrl: '/web',
 };
