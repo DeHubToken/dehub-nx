37fe695bd0670ae4170f39b659c2585d3ba743c6
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2021-11-30 18:16:13 +0100

    jest: fix

--- jest.preset.js
+++ jest.preset.js
@@ -1,12 +1,3 @@
 const nxPreset = require('@nrwl/jest/preset');
 
-module.exports = {
-  ...nxPreset,
-  moduleNameMapper: {
-    '^lodash-es$': 'lodash',
-    '^@dehub/shared/assets/dehub/(.*)$':
-      '<rootDir>/../../libs/shared/assets/dehub/src/lib/$1',
-    '^@dehub/shared/assets/freya/(.*)$':
-      '<rootDir>/../../libs/shared/assets/freya/src/lib/$1',
-  },
-};
+module.exports = { ...nxPreset };
