25551074bf34299f8c9bfbc0b546b8b169098dd4
Ben Weider <84747155+deep-quality-dev@users.noreply.github.com>
2021-12-02 18:50:56 +0800

    feat(prediction): introduce duotune icons in react fontawesome (#110)
    * feat(prediction): introduce duotune icons in react fontawesome
    
    * Revert "Auxiliary commit to revert individual files from e08c8f0a8b4795b6cfb3d41e83f5d532bb4641ad"
    
    This reverts commit afd8f7b2515a117fc5009a70aebef9f33ecd7109.

--- apps/prediction/src/app/views/Predictions/components/PrevNextNav.tsx
+++ apps/prediction/src/app/views/Predictions/components/PrevNextNav.tsx
@@ -6,9 +6,12 @@ import {
   IconButton,
 } from '@dehub/react/pcsuikit';
 import styled from 'styled-components';
+import { faHome } from '@fortawesome/pro-duotone-svg-icons';
+
 import { useGetCurrentEpoch, useGetSortedRounds } from '../../../state/hooks';
 import useSwiper from '../hooks/useSwiper';
-import { color } from 'styled-system';
+import DuotoneFontAwesomeIcon from './DuotoneFontAwesomeIcon';
+import useTheme from '../../../hooks/useTheme';
 
 const StyledPrevNextNav = styled(Card)`
   align-items: center;
@@ -27,6 +30,7 @@ const PrevNextNav = () => {
   const { swiper } = useSwiper();
   const currentEpoch = useGetCurrentEpoch();
   const rounds = useGetSortedRounds();
+  const { theme } = useTheme();
 
   const handlePrevSlide = () => {
     swiper?.slidePrev();
@@ -51,7 +55,21 @@ const PrevNextNav = () => {
         <ArrowBackIcon color="secondary" width="24px" />
       </IconButton>
       <IconButton variant="primary" scale="sm" onClick={handleSlideToLive}>
-        <i className="fad fa-home"></i>
+        {/* <i className="fad fa-home"></i> */}
+        {/* <DuotoneFontAwesomeIcon
+          icon={faHome}
+          primaryColor="#f11484"
+          primaryOpacity="0.8"
+          secondaryColor="rgb(255,216,0)"
+          secondaryOpacity="0.8"
+        /> */}
+        <DuotoneFontAwesomeIcon
+          icon={faHome}
+          primaryColor={theme.colors.primary}
+          primaryOpacity="0.8"
+          secondaryColor={theme.colors.inputSecondary}
+          secondaryOpacity="0.8"
+        />
       </IconButton>
       <IconButton variant="text" scale="sm" onClick={handleNextSlide}>
         <ArrowForwardIcon color="secondary" width="24px" />
