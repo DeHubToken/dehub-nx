ce386ae2dbcd1cd19b60107c746462e6b3ef63c7
Aleksandr Pasevin <pasevin@gmail.com>
2021-12-02 18:04:08 +0100

    constants(prediction): added mainnet contract address

--- libs/shared/config/src/lib/constants.ts
+++ libs/shared/config/src/lib/constants.ts
@@ -69,7 +69,7 @@ export const ContractAddresses: {
     'BNB-BUSD': '0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16',
     StandardLottery: '0xF5A881B2c1Bc20DBE8E54674d65e8D66487da35e',
     SpecialLottery: '0xdA9F2a546AfF5deDCF464205B229d18ab35B2d22',
-    Prediction: '0x05f3c9928df942e734bbb5b8027b063f8f308b48',
+    Prediction: '0xC09411b2eA0aBd5532170585b3AbBF7d6A7c500b',
     ChainLinkOracle: '0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE',
     MultiCall: '0xfF6FD90A470Aaa0c1B8A54681746b07AcdFedc9B',
   },
