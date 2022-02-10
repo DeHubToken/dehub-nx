8866805b962a7872a9ce3a629057bac4d63aed78
deep-quality-dev <hungrywarrior081@gmail.com>
2022-02-10 22:53:12 +1000

    refactor(staking): remove common files

--- apps/staking/src/app/Providers.tsx
+++ apps/staking/src/app/Providers.tsx
@@ -1,4 +1,4 @@
-import { RefreshContextProvider } from '@dehub/react/core';
+import { ConnectProvider, RefreshContextProvider } from '@dehub/react/core';
 import React from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import { MoralisProvider } from 'react-moralis';
@@ -6,7 +6,10 @@ import { Provider } from 'react-redux';
 import { environment } from '../environments/environment';
 import store from './state';
 
+const { landing } = environment.dehub;
 const { appId, serverUrl } = environment.moralis;
+const { chainId } = environment.web3;
+const { baseUrl } = environment;
 
 const Providers: React.FC = ({ children }) => {
   return (
@@ -17,7 +20,14 @@ const Providers: React.FC = ({ children }) => {
     >
       <Provider store={store}>
         <RefreshContextProvider>
-          <HelmetProvider>{children}</HelmetProvider>
+          <ConnectProvider
+            defaultChainId={chainId}
+            baseUrl={baseUrl}
+            pageTitle={'DeHub Staking'}
+            landingUrl={landing}
+          >
+            <HelmetProvider>{children}</HelmetProvider>
+          </ConnectProvider>
         </RefreshContextProvider>
       </Provider>
     </MoralisProvider>
--- apps/staking/src/app/views/SyncWaiting.tsx
+++ apps/staking/src/app/views/SyncWaiting.tsx
@@ -1,7 +1,5 @@
+import { Container, Heading, Text } from '@dehub/react/ui';
 import { Card } from 'primereact/card';
-
-import Container from '../components/Layout/Container';
-import { Header, Text } from '../components/Text';
 import { LoadingStatus } from '../config/constants/types';
 
 const SyncWaiting = ({ loadingStatus }: { loadingStatus: LoadingStatus }) => {
@@ -13,24 +11,24 @@ const SyncWaiting = ({ loadingStatus }: { loadingStatus: LoadingStatus }) => {
             <div className="fa-3x pb-5 text-center">
               <i className="fad fa-circle-notch fa-spin"></i>
             </div>
-            <Header className="text-center">Loading</Header>
+            <Heading className="text-center">Loading</Heading>
           </>
         ) : loadingStatus === LoadingStatus.SYNCHRONIZING ? (
           <>
             <div className="fa-3x pb-5 text-center">
               <i className="fad fa-circle-notch fa-spin"></i>
             </div>
-            <Header className="text-center">
+            <Heading className="text-center">
               Syncing with $DeHub Contract
-            </Header>
+            </Heading>
             <Text className="text-center">Please give it a second.</Text>
           </>
         ) : loadingStatus === LoadingStatus.PAUSED ? (
-          <Header className="text-center">
+          <Heading className="text-center">
             staking is paused for a while. Please wait...
-          </Header>
+          </Heading>
         ) : (
-          <Header className="text-center">Welcome to DeHub staking!</Header>
+          <Heading className="text-center">Welcome to DeHub staking!</Heading>
         )}
       </Card>
     </Container>
