215c3b7f6b69d12ff4035e5df5de95453013f5ff
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2022-02-15 11:39:56 +0100

    fix(prediction): fix lint issues

--- apps/prediction/src/app/app.tsx
+++ apps/prediction/src/app/app.tsx
@@ -1,23 +1,14 @@
-import {
-  useConnectContext,
-  useEagerMoralis,
-  withLayout,
-} from '@dehub/react/core';
+import { useConnectContext, useEagerMoralis } from '@dehub/react/core';
 import { ResetCSS } from '@dehub/react/pcsuikit';
 import { FullScreenLoader, SuspenseWithChunkError } from '@dehub/react/ui';
 import BigNumber from 'bignumber.js';
-import { lazy } from 'react';
 import { Route, Router, Switch } from 'react-router-dom';
-import { environment } from '../environments/environment';
 import EasterEgg from './components/EasterEgg';
 import ToastListener from './components/ToastListener';
 import history from './routerHistory';
 import { usePollBlockNumber } from './state/hooks';
 import GlobalStyle from './style/Global';
-
-const Predictions = withLayout(lazy(() => import('./views/Predictions')));
-
-const { baseUrl } = environment;
+import Predictions from './views/Predictions';
 
 // This config is required for number formatting
 BigNumber.config({
