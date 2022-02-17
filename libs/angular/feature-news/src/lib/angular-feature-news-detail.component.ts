0f39d3e56ec964197073e4864ca39ba881add4d7
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2022-02-17 18:39:10 +0100

    feat(web): early working basicPost detail draft on news

--- libs/angular/feature-news/src/lib/angular-feature-news-detail.component.ts
+++ libs/angular/feature-news/src/lib/angular-feature-news-detail.component.ts
@@ -1,6 +1,24 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Inject,
+  OnInit,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {
+  documentToHtmlString,
+  Options,
+} from '@contentful/rich-text-html-renderer';
+import { BLOCKS } from '@contentful/rich-text-types';
+import {
+  BasicPostCollectionBySlugService,
+  EnvToken,
+} from '@dehub/angular/core';
+import { SharedEnv } from '@dehub/shared/config';
+import { BasicPostDetailFragment } from '@dehub/shared/model';
 import { bounceInRightOnEnterAnimation } from 'angular-animations';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   template: `
@@ -11,18 +29,77 @@ import { bounceInRightOnEnterAnimation } from 'angular-animations';
         styleClass="p-button-link p-mr-2"
       ></p-button>
 
-      <h3 [@bounceInRight] class="col-12">{{ slug }}</h3>
+      <ng-container *ngIf="basicPostDetail$ | async as basicPostDetail">
+        <div
+          [dhbContentfulDraft]="basicPostDetail.sys"
+          class="card image-card shadow-8 mx-4"
+        >
+          <!-- Main Picture -->
+          <ng-container *ngIf="basicPostDetail.mainPicture as mainPicture">
+            <img [src]="mainPicture.url" [alt]="mainPicture.title" />
+          </ng-container>
+
+          <div class="image-content">
+            <!-- Title -->
+            <h3>{{ basicPostDetail.title }}</h3>
+
+            <!-- Date -->
+            <p>
+              {{
+                basicPostDetail.sys.publishedAt
+                  | date: 'EEE, MMM d, y, hh:mm:ss zzzz'
+              }}
+            </p>
+
+            <!-- Description -->
+            <div
+              [innerHtml]="getRichMarkup(basicPostDetail) | dhbSafeHtml"
+              class="line-height-3"
+            ></div>
+          </div>
+        </div>
+      </ng-container>
     </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [bounceInRightOnEnterAnimation({ anchor: 'bounceInRight' })],
 })
 export class AngularFeatureNewsDetailComponent implements OnInit {
-  slug?: string;
+  basicPostDetail$!: Observable<BasicPostDetailFragment | undefined>;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    @Inject(EnvToken) private env: SharedEnv,
+    private route: ActivatedRoute,
+    private basicPostDetailsBySlugService: BasicPostCollectionBySlugService
+  ) {}
 
   ngOnInit() {
-    this.slug = this.route.snapshot.paramMap.get('slug') ?? undefined;
+    const slug = this.route.snapshot.paramMap.get('slug') ?? undefined;
+
+    this.basicPostDetail$ = this.basicPostDetailsBySlugService
+      .fetch({
+        slug,
+        isPreview: this.env.contentful.isPreview,
+      })
+      .pipe(
+        map(
+          ({ data: { basicPostCollection } }) =>
+            basicPostCollection?.items[0] ?? undefined
+        )
+      );
+  }
+
+  /** TODO: this can be a util */
+  getRichMarkup({ description }: BasicPostDetailFragment) {
+    const richOptions: Options = {
+      renderNode: {
+        [BLOCKS.HEADING_5]: (node, next) => `<h5>${next(node.content)}</h5>`,
+        [BLOCKS.HEADING_6]: (node, next) => `<h6>${next(node.content)}</h6>`,
+        [BLOCKS.UL_LIST]: (node, next) => `<ul>${next(node.content)}</ul>`,
+        [BLOCKS.OL_LIST]: (node, next) => `<ol>${next(node.content)}</ol>`,
+        [BLOCKS.LIST_ITEM]: (node, next) => `<li>${next(node.content)}</li>`,
+      },
+    };
+    return documentToHtmlString(description?.json, richOptions);
   }
 }
