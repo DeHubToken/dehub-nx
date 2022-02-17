2cc88be5c863950c412a85eefb28d22463e0174c
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2022-02-17 12:35:09 +0100

    feat(web): feature-news lib skeleton

---
+++ libs/angular/feature-news/src/lib/angular-feature-news.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Route } from '@angular/router';
+
+export const angularFeatureNewsRoutes: Route[] = [];
+
+@NgModule({
+  imports: [CommonModule, RouterModule],
+})
+export class AngularFeatureNewsModule {}
