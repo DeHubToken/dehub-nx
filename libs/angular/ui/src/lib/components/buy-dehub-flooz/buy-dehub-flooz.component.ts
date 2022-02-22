65d44a3010abea402e6204760402953c825d19cc
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2022-02-22 19:30:25 +0100

    perf(iframe): the browser delays the loading of the offscreen iframes until the user scrolls near them

--- libs/angular/ui/src/lib/components/buy-dehub-flooz/buy-dehub-flooz.component.ts
+++ libs/angular/ui/src/lib/components/buy-dehub-flooz/buy-dehub-flooz.component.ts
@@ -5,6 +5,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
     <iframe
       src="https://www.flooz.trade/embedded/0xFC206f429d55c71cb7294EfF40c6ADb20dC21508/?refId=AbAsFD&backgroundColor=linear-gradient(45deg%2Crgba(11%2C17%2C19%2C0.7)%2C%20rgba(5%2C17%2C24%2C0.7)%2046%25%2C%20rgba(6%2C12%2C29%2C0.7)%2071%25%2C%20rgba(50%2C19%2C56%2C0.7))"
       allow="clipboard-read; clipboard-write; web-share; accelerometer *; autoplay *; camera *; gyroscope *; payment *"
+      loading="lazy"
       title="Flooz Trade"
       width="100%"
       height="640"
