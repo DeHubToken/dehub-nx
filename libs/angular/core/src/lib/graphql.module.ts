ec593f546afeff9cf00cb356e82d9a038e50a892
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2022-02-25 08:46:21 +0100

    feat(graphql): handle Contentful unresolved link errors in case of draft entries in prod

--- libs/angular/core/src/lib/graphql.module.ts
+++ libs/angular/core/src/lib/graphql.module.ts
@@ -6,6 +6,7 @@ import { onError } from '@apollo/client/link/error';
 import { SharedEnv } from '@dehub/shared/config';
 import { ApolloModule, APOLLO_FLAGS, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
+import { GraphQLErrorExtensions } from 'graphql';
 import { EnvToken } from './models/injection-token';
 
 /**
@@ -27,13 +28,50 @@ export function createApollo(
 ) {
   return {
     link: from([
-      onError(({ graphQLErrors, networkError }) => {
+      onError(({ graphQLErrors, networkError, response }) => {
+        /**
+         * Check whether Contentful unresolved link error happen
+         *
+         * This happens if an entry is set to unpublish or not published yet,
+         * but assigned to az existing other published entry.
+         *
+         * Only happens in case of preview mode on (prod + CDA).
+         * In this case the draft entries wont't be retrieved,
+         * but GQL response will contain data and also errors properties
+         *
+         * @param extError the Contentful extension error
+         * @returns true in case of unpublished link error which cause unresolved entry in prod (CDA)
+         */
+        const isContentfulUnresolvableLink = (
+          extError: GraphQLErrorExtensions
+        ) =>
+          (extError['contentful'] as { code: string }).code ===
+          'UNRESOLVABLE_LINK';
+
         if (graphQLErrors) {
-          graphQLErrors.map(({ message, locations, path }) =>
-            console.error(
-              `[GraphQL error]: ${message}, Location: ${locations}, Path: ${path}`
-            )
-          );
+          graphQLErrors.map(({ message, locations, path, extensions }) => {
+            const isUnresolvedLinkError =
+              isContentfulUnresolvableLink(extensions);
+
+            const msg = `[GraphQL ${
+              isUnresolvedLinkError ? 'warn' : 'error'
+            }]: ${message}, Extension: ${JSON.stringify(
+              extensions,
+              undefined,
+              ''
+            )}, Location: ${locations}, Path: ${path}`;
+
+            // Only show warnings in production for unresolved links
+            isUnresolvedLinkError ? console.warn(msg) : console.error(msg);
+          });
+
+          // Ignore errors which are just contentful unresolved link related
+          if (response && response.errors) {
+            const remainingErrors = response.errors.filter(
+              error => !isContentfulUnresolvableLink(error.extensions)
+            );
+            if (remainingErrors.length === 0) delete response.errors;
+          }
         }
         if (networkError)
           console.error(
