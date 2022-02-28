04d5e7aa49e11628f6851fa82320b755322c6dcd
Aleksandr Pasevin <pasevin@gmail.com>
2022-02-28 18:52:47 +0300

    fix(web): lint issue

--- libs/angular/feature-earn/src/lib/angular-feature-earn.module.ts
+++ libs/angular/feature-earn/src/lib/angular-feature-earn.module.ts
@@ -2,8 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PageSectionsModule } from '@dehub/angular/ui/components/page-sections';
 import { ContentfulDraftDirectiveModule } from '@dehub/angular/ui/directives/contentful-draft';
-import { AngularFeatureEarnRoutingModule } from 'libs/angular/feature-earn/src/lib/angular-feature-earn-routing.module';
-import { AngularFeatureEarnComponent } from 'libs/angular/feature-earn/src/lib/angular-feature-earn.component';
+import { AngularFeatureEarnRoutingModule } from './angular-feature-earn-routing.module';
+import { AngularFeatureEarnComponent } from './angular-feature-earn.component';
 
 @NgModule({
   imports: [
