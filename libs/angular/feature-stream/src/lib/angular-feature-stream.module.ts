0e4d3a95823e6d839acc9e447b65867bc97a90f8
Aleksandr Pasevin <pasevin@gmail.com>
2022-02-28 23:13:35 +0300

    feat(web): add sections to stream page

--- libs/angular/feature-stream/src/lib/angular-feature-stream.module.ts
+++ libs/angular/feature-stream/src/lib/angular-feature-stream.module.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { PageSectionsModule } from '@dehub/angular/ui/components/page-sections';
+import { ContentfulDraftDirectiveModule } from '@dehub/angular/ui/directives/contentful-draft';
 import { AngularFeatureStreamRoutingModule } from './angular-feature-stream-routing.module';
+import { AngularFeatureStreamComponent } from './angular-feature-stream.component';
 
 @NgModule({
   imports: [
@@ -8,10 +11,13 @@ import { AngularFeatureStreamRoutingModule } from './angular-feature-stream-rout
     CommonModule,
 
     // Lib Modules
+    ContentfulDraftDirectiveModule,
+    PageSectionsModule,
 
     // PrimeNg Modules
 
     AngularFeatureStreamRoutingModule,
   ],
+  declarations: [AngularFeatureStreamComponent],
 })
 export class AngularFeatureStreamModule {}
