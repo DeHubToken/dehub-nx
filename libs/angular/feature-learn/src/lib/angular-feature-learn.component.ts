fbb8727a051f195ba985071c361ebc495613a4cb
Aleksandr Pasevin <pasevin@gmail.com>
2022-03-01 13:40:47 +0300

    refactor(web): clean up learn page

--- libs/angular/feature-learn/src/lib/angular-feature-learn.component.ts
+++ libs/angular/feature-learn/src/lib/angular-feature-learn.component.ts
@@ -6,10 +6,7 @@ import {
 } from '@angular/core';
 import { EnvToken, PageLearnCollectionService } from '@dehub/angular/core';
 import { SharedEnv } from '@dehub/shared/config';
-import {
-  PageLearnFragment,
-  SwiperResponsiveOptions,
-} from '@dehub/shared/model';
+import { PageLearnFragment } from '@dehub/shared/model';
 import { bounceInLeftOnEnterAnimation } from 'angular-animations';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -26,8 +23,6 @@ import { map } from 'rxjs/operators';
       <!-- Page Sections -->
       <dhb-page-sections
         [sections]="pageLearn.sectionsCollection?.items"
-        [featurePostsResponsiveOptions]="featurePostsResponsiveOptions"
-        [basicPostsResponsiveOptions]="basicPostsResponsiveOptions"
       ></dhb-page-sections>
     </ng-container>
   `,
@@ -37,48 +32,6 @@ import { map } from 'rxjs/operators';
 })
 export class AngularFeatureLearnComponent implements OnInit {
   pageLearn$?: Observable<PageLearnFragment | undefined>;
-
-  featurePostsResponsiveOptions: SwiperResponsiveOptions = {
-    '1800': {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-    '1250': {
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-    '320': {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    },
-  };
-
-  basicPostsResponsiveOptions: SwiperResponsiveOptions = {
-    '1900': {
-      slidesPerView: 6,
-      spaceBetween: 20,
-    },
-    '1700': {
-      slidesPerView: 5,
-      spaceBetween: 20,
-    },
-    '1350': {
-      slidesPerView: 4,
-      spaceBetween: 20,
-    },
-    '960': {
-      slidesPerView: 3,
-      spaceBetween: 20,
-    },
-    '750': {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    '320': {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    },
-  };
 
   constructor(
     @Inject(EnvToken) private env: SharedEnv,
