20410d9b0b5810707b6048bce568b8178737f5f8
Aleksandr Pasevin <pasevin@gmail.com>
2022-03-01 11:25:12 +0300

    fix(web): lint issue

--- libs/angular/ui/src/lib/components/grand-post/grand-post.component.module.ts
+++ libs/angular/ui/src/lib/components/grand-post/grand-post.component.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { GrandPostComponent } from '@dehub/angular/ui/components/grand-post/grand-post.component';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { ContentfulDraftDirectiveModule } from '../../directives/contentful-draft';
 import { YoutubeVideoIdPipeModule } from '../../pipes/youtube-video-id';
+import { GrandPostComponent } from '../grand-post/grand-post.component';
 import { YoutubeEmbedModule } from '../youtube-embed';
 
 @NgModule({
