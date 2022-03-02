18ed9a4491ab75e6d54c2adedb76dc7181466682
Aleksandr Pasevin <pasevin@gmail.com>
2022-03-02 13:07:44 +0300

    style(web): increase section post section margin

--- libs/angular/ui/src/lib/components/page-section-section-posts/page-section-section-posts.component.ts
+++ libs/angular/ui/src/lib/components/page-section-section-posts/page-section-section-posts.component.ts
@@ -18,7 +18,7 @@ import { fadeInUpOnEnterAnimation } from 'angular-animations';
       *ngIf="section"
       [dhbContentfulDraft]="section.sys"
       [@fadeInUp]
-      class="col-12 sm:col-12 md:col-8 xl:col-8 col-offset-0 sm:col-offset-0 md:col-offset-2 xl:col-offset-2 flex flex-column mb-5"
+      class="col-12 sm:col-12 md:col-8 xl:col-8 col-offset-0 sm:col-offset-0 md:col-offset-2 xl:col-offset-2 flex flex-column mb-8"
     >
       <h3 *ngIf="section.title as title">{{ title }}</h3>
       <h5
