0922ea45f461e5264ee03ed0dacdc85bb6e0ffe5
Aleksandr Pasevin <pasevin@gmail.com>
2022-03-18 09:39:52 +0300

    style(staking): header and other small details (#222)
    * style(staking): responsive flex class names on layout-topbar-wrapper
    
    * style(staking): change main title
    
    * feat(staking): add usd value to “Your Stake” section

--- libs/react/ui/src/lib/Header/Header.tsx
+++ libs/react/ui/src/lib/Header/Header.tsx
@@ -11,7 +11,7 @@ function Header({ userMenu, logo }: HeaderProps) {
   return (
     <div className="layout-topbar-dark">
       <div className="layout-topbar">
-        <div className="layout-topbar-wrapper">
+        <div className="layout-topbar-wrapper flex-column sm:flex-row">
           <div className="layout-topbar-left">
             <Logo logo={logo} />
           </div>
