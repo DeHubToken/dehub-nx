f90bcca2d5195bd1b8a3ae0a6c0f14dbe35f7048
deep-quality-dev <hungrywarrior081@gmail.com>
2022-03-21 18:11:28 +1000

    style(staking): padding after skeleton

--- apps/staking/src/app/views/components/LiveCard.tsx
+++ apps/staking/src/app/views/components/LiveCard.tsx
@@ -353,7 +353,7 @@ const LiveCard = () => {
                         </Text>
                       )
                     ) : (
-                      <Skeleton width="100%" height="1.5rem" className="mt-2" />
+                      <Skeleton width="100%" height="1.5rem" className="my-2" />
                     )}
 
                     {fetchRewardStatus === FetchStatus.SUCCESS &&
