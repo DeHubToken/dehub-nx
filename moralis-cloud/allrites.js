637c586af5b5cb512a21940c72cc4a70e0ad14e4
deep-quality-dev <hungrywarrior081@gmail.com>
2022-03-21 23:43:59 +1000

    feat(moralis-cloud): add `getDeHubContracts` which returns contract information per dapp

--- moralis-cloud/allrites.js
+++ moralis-cloud/allrites.js
@@ -88,6 +88,57 @@ async function getDeHubStakingConfig(chainId) {
 }
 
 /*******************************************************
+ * DeHub contracts
+ ******************************************************/
+/**
+ * Get contract properties and address/abis
+ * @returns array of contract, every contract has 4 common properties
+ * they are: address, name, chainId, abi
+ */
+async function getDeHubContracts(dappName) {
+  const logger = Moralis.Cloud.getLogger();
+  try {
+    const Contracts = Moralis.Object.extend('Contracts');
+    const ContractABIs = Moralis.Object.extend('ContractABIs');
+    const DeHubDapp = Moralis.Object.extend(dappName);
+
+    const contractQuery = new Moralis.Query(Contracts);
+    const abisQuery = new Moralis.Query(ContractABIs);
+    const dappQuery = new Moralis.Query(DeHubDapp);
+
+    contractQuery.matchesQuery('abi', abisQuery);
+    contractQuery.include('abi');
+    dappQuery.matchesQuery('contract', contractQuery);
+    dappQuery.include('contract');
+
+    const contracts = await dappQuery.find();
+    for (let i = 0; i < contracts.length; i++) {
+      const contract = contracts[i].get('contract');
+      // Moralis does not support over 2 depths relational queries
+      // so we need to query one by one for abis
+      const abiR = contract.get('abi'); // typeof Table Object
+      abisQuery.equalTo('objectId', abiR.id);
+      const abiQ = await abisQuery.find();
+      contract.set('abi', abiQ[0].get('abi'));
+
+      const address = contract.get('address');
+      const name = contract.get('name');
+      const chainId = contract.get('chainId');
+      const abi = contract.get('abi');
+
+      contracts[i].set('address', address);
+      contracts[i].set('name', name);
+      contracts[i].set('chainId', chainId);
+      contracts[i].set('abi', abi);
+    }
+    return contracts;
+  } catch (err) {
+    logger.error(`getDeHubContracts error: ${JSON.stringify(err)}`);
+  }
+  return null;
+}
+
+/*******************************************************
  * Query on Moralis Database
  ******************************************************/
 /**
@@ -317,5 +368,38 @@ Moralis.Cloud.beforeLogin(async request => {
   } catch (err) {
     logger.error(`beforeLogin error: ${JSON.stringify(err)}`);
     return;
+  }
+});
+
+Moralis.Cloud.define('getStakingContracts', async request => {
+  const logger = Moralis.Cloud.getLogger();
+  try {
+    const contracts = await getDeHubContracts('DeHubStakingDapp');
+
+    let collect = [];
+    for (let i = 0; i < contracts.length; i++) {
+      const year = contracts[i].get('year');
+      const quarter = contracts[i].get('quarter');
+
+      const contract = contracts[i].get('contract');
+      const address = contract.get('address');
+
+      const name = contract.get('name');
+      const chainId = contract.get('chainId');
+      const abi = contract.get('abi');
+
+      collect.push({
+        year,
+        quarter,
+        address,
+        name,
+        chainId,
+        abi,
+      });
+    }
+    return collect;
+  } catch (err) {
+    logger.error(`getStakingContracts error: ${JSON.stringify(err)}`);
+    return null;
   }
 });
