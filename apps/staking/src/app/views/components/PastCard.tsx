17ef9d27d60fb3154179496a5ba35fd3db143ef0
Aleksandr Pasevin <pasevin@gmail.com>
2022-03-24 15:22:24 +0300

    refactor(staking): update the style of PastCard

--- apps/staking/src/app/views/components/PastCard.tsx
+++ apps/staking/src/app/views/components/PastCard.tsx
@@ -83,21 +83,21 @@ const PastCard = ({ poolIndex }: CardProps) => {
     <>
       <Toast ref={toast} />
 
-      <Card className="border-neon-2 overflow-hidden mt-5">
+      <Card className="border-neon-1 overflow-hidden mt-5">
         <StyledBox>
           <Heading
-            className="py-2 px-3 inline-flex border-neon-2"
+            className="py-2 px-3 inline-flex border-neon-1"
             style={{
               borderRadius: '8px',
               background:
-                'linear-gradient(50deg, rgba(89,70,0,1) 0%, rgba(193,160,49,1) 48%, rgba(89,70,0,1) 100%)',
+                'linear-gradient(50deg, rgba(46,59,78,1) 0%, rgba(158,198,223,1) 48%, rgba(46,59,78,1) 100%)',
             }}
           >
             <span style={{ fontWeight: 900 }}>{quarterMark(poolInfo)}</span>
           </Heading>
 
           <div className="grid mt-2">
-            <div className="col-12 md:col-6 lg:col-6 align-self-start">
+            <div className="col-12 align-self-start">
               <div className="card overview-box gray shadow-2">
                 <div className="overview-info text-left w-full">
                   <Heading className="pb-1">Your Stake</Heading>
@@ -127,10 +127,10 @@ const PastCard = ({ poolIndex }: CardProps) => {
                 </div>
               </div>
             </div>
-            <div className="col-12 md:col-6 lg:col-6 align-self-start">
+            <div className="col-12 align-self-start">
               <div className="card overview-box gray shadow-2">
                 <div className="overview-info text-left w-full">
-                  <Heading className="pb-1">Rewards</Heading>
+                  <Heading className="pb-1">Withdrawable Rewards</Heading>
                   {account && pendingHarvest ? (
                     <>
                       <Text fontSize="24px" fontWeight={900}>
@@ -151,7 +151,7 @@ const PastCard = ({ poolIndex }: CardProps) => {
                 </div>
               </div>
             </div>
-            <div className="col-12 md:col-6 lg:col-6 align-self-start">
+            <div className="col-12 align-self-start">
               {account ? (
                 <Button
                   className="p-button mt-2 justify-content-center"
