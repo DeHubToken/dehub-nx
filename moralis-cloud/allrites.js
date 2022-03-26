5a9096390b88f21b3a665eb9e714e359c6c14c36
deep-quality-dev <hungrywarrior081@gmail.com>
2022-03-27 00:36:23 +1000

    fix(moralis-cloud): update `can_play` only with staked amount

--- moralis-cloud/allrites.js
+++ moralis-cloud/allrites.js
@@ -290,15 +290,11 @@ async function updateCanPlay(chainId, address) {
       logger.error(`Not found Moralis User: ${address}`);
       return;
     }
-    const balance = await getDeHubTokenBalance(chainId, address);
-    logger.info(`user DeHub balance(${address}): ${balance.toString()}`);
     const staked = await getStakedAmount(chainId, address);
     logger.info(`user staked DeHub amount(${address}): ${staked.toString()}`);
-    const total = balance.plus(staked);
-    logger.info(`user total DeHub balance(${address}): ${total.toString()}`);
 
     const minAmount = await getOTTMinTokensToPlay();
-    await setCanPlay(user, total.gte(minAmount) ? true : false);
+    await setCanPlay(user, staked.gte(minAmount) ? true : false);
   } catch (err) {
     logger.error(`updateCanPlay error: ${JSON.stringify(err)}`);
     return;
@@ -311,23 +307,6 @@ async function updateCanPlay(chainId, address) {
  * The updates on this variable will automatically trigger calling to Allrites
  * so as to give an access to user
  */
-Moralis.Cloud.afterSave('DeHubTokenTransferEvents', async request => {
-  const logger = Moralis.Cloud.getLogger();
-  try {
-    const fromAddress = request.object.get('from');
-    const toAddress = request.object.get('to');
-    logger.info(
-      `Noticed DeHubTokenTransferEvents, from: ${fromAddress}, to: ${toAddress}`
-    );
-
-    await updateCanPlay(defChainId, fromAddress);
-    await updateCanPlay(defChainId, toAddress);
-  } catch (err) {
-    logger.error(`DeHubTokenTransferEvents error: ${JSON.stringify(err)}`);
-    return;
-  }
-});
-
 Moralis.Cloud.afterSave('DeHubStakingDepositEvents', async request => {
   const logger = Moralis.Cloud.getLogger();
   try {
