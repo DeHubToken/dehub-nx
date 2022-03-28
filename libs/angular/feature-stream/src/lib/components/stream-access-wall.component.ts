f7513da88dd60aa372e3fc1acf96d9bfbdc1bc4c
Aleksandr Pasevin <pasevin@gmail.com>
2022-03-28 23:10:09 +0300

    feat(web): completed stream access wall

--- libs/angular/feature-stream/src/lib/components/stream-access-wall.component.ts
+++ libs/angular/feature-stream/src/lib/components/stream-access-wall.component.ts
@@ -8,53 +8,13 @@ import { EnvToken, PageAccessWallCollectionService } from '@dehub/angular/core';
 import { BuyDehubFloozComponent } from '@dehub/angular/ui/components/buy-dehub-flooz';
 import { SharedEnv } from '@dehub/shared/config';
 import { PageAccessWallFragment } from '@dehub/shared/model';
+import { WINDOW } from '@ng-web-apis/common';
 import { fadeInUpOnEnterAnimation } from 'angular-animations';
 import { DialogService } from 'primeng/dynamicdialog';
 import { map, Observable } from 'rxjs';
 
 @Component({
-  template: `
-    <ng-container
-      *ngIf="pageStreamAccessWall$ | async as pageStreamAccessWall"
-      class="grid"
-    >
-      <!-- Titles -->
-      <dhb-page-header [page]="pageStreamAccessWall"></dhb-page-header>
-
-      <!-- Hardcoded Instructions -->
-      <div
-        [@fadeInUp]
-        class="col-12 sm:col-12 md:col-8 xl:col-8 col-offset-0 sm:col-offset-0 md:col-offset-2 xl:col-offset-2"
-      >
-        <p-fieldset
-          [classList]="'bg-gradient-2-propagate border-neon-1-propagate'"
-        >
-          <ng-template pTemplate="header">
-            <i
-              class="fa-duotone fa-square-1 icon-color-duotone-3 text-4xl pr-3"
-            ></i>
-            <h4 class="inline">Buy DeHub</h4>
-          </ng-template>
-          <p class="text-lg">
-            First you need to own DeHub tokens. Don't worry it's very easy to
-            do. Just follow our quidelines here if you're new to this.
-          </p>
-          <dhb-buy-dehub-button
-            [cexUrl]="cexUrl"
-            [downloadWalletUrl]="downloadWalletUrl"
-            (buy)="onDexSelected()"
-            (dexSelected)="onDexSelected()"
-          ></dhb-buy-dehub-button>
-        </p-fieldset>
-      </div>
-
-      <!-- Page Sections -->
-      <dhb-page-sections
-        [sections]="pageStreamAccessWall.sectionsCollection?.items"
-        [path]="path"
-      ></dhb-page-sections>
-    </ng-container>
-  `,
+  templateUrl: './stream-access-wall.component.html',
   styles: [
     `
       i,
@@ -75,6 +35,7 @@ export class StreamAccessWallComponent implements OnInit {
 
   constructor(
     @Inject(EnvToken) private env: SharedEnv,
+    @Inject(WINDOW) readonly windowRef: Window,
     private pageAccessWallCollectionService: PageAccessWallCollectionService,
     private dialogService: DialogService
   ) {}
@@ -101,5 +62,14 @@ export class StreamAccessWallComponent implements OnInit {
       closeOnEscape: true,
       dismissableMask: true,
     });
+  }
+
+  onWatchClicked(event: Event) {
+    event.preventDefault();
+    this.windowRef.open(
+      'https://beta-stream.dehub.net/',
+      '_blank',
+      'noopener,noreferrer'
+    );
   }
 }
