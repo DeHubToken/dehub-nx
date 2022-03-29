45203402320f7be501be05a37aa9a25fc563e74b
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2022-03-29 15:58:24 +0200

    revert: empty ott access wall routes

--- libs/angular/feature-stream/src/lib/angular-feature-stream-routing.module.ts
+++ libs/angular/feature-stream/src/lib/angular-feature-stream-routing.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AngularFeatureStreamComponent } from './angular-feature-stream.component';
-import { StreamAccessWallComponent } from './components/stream-access-wall.component';
-import { CanPlayGuard } from './guards';
 
 const routes: Routes = [
   {
     path: '',
     component: AngularFeatureStreamComponent,
-    canActivate: [CanPlayGuard],
+    // canActivate: [CanPlayGuard],
   },
-  {
-    path: 'access-wall',
-    component: StreamAccessWallComponent,
-  },
+  // {
+  //   path: 'access-wall',
+  //   component: StreamAccessWallComponent,
+  // },
 ];
 
 @NgModule({
