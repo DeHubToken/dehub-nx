34ff8e60773f3e35fb41371d775ff8edf6a5bbee
Aleksandr Pasevin <pasevin@gmail.com>
2022-04-07 16:28:43 +0300

    fix(web): disable no explicit any lint check on the pipe which literally deals with any types

--- libs/angular/ui/src/lib/pipes/as-pipe/as.pipe.ts
+++ libs/angular/ui/src/lib/pipes/as-pipe/as.pipe.ts
@@ -5,6 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
   pure: true,
 })
 export class AsPipe implements PipeTransform {
+  // Reason: this pipe can accept literally any type.
+  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
   transform<T>(value: any, _type: (new (...args: any[]) => T) | T): T {
     return value as T;
   }
