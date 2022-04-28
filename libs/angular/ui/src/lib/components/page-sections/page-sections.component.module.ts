bb1ccfd23a2b2fde4c90d80635b530853e7ad40c
Aleksandr Pasevin <pasevin@gmail.com>
2022-04-28 11:53:52 +0300

    fix(web): relative import lint requirement

--- libs/angular/ui/src/lib/components/page-sections/page-sections.component.module.ts
+++ libs/angular/ui/src/lib/components/page-sections/page-sections.component.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { PageSectionGrandPostsSwiperModule } from '@dehub/angular/ui/components/page-section-grand-posts-swiper';
 import { PageSectionBasicPostsModule } from '../page-section-basic-posts';
 import { PageSectionDappPostsModule } from '../page-section-dapp-posts';
 import { PageSectionFaQsModule } from '../page-section-faqs';
 import { PageSectionFeaturePostsModule } from '../page-section-feature-posts';
 import { PageSectionGrandPostsModule } from '../page-section-grand-posts';
+import { PageSectionGrandPostsSwiperModule } from '../page-section-grand-posts-swiper';
 import { PageSectionIconTilesModule } from '../page-section-icon-tiles';
 import { PageSectionIconTilesSwiperModule } from '../page-section-icon-tiles-swiper';
 import { PageSectionPersonPostsModule } from '../page-section-person-posts';
