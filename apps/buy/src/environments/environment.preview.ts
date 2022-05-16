a653041f8a180179406cc7df07708722c6596cf9
deep-quality-dev <hungrywarrior081@gmail.com>
2022-05-16 02:14:16 -0400

    chore(react): preview environment and project configuration

---
+++ apps/buy/src/environments/environment.preview.ts
@@ -0,0 +1,19 @@
+import { defaultSharedPreviewEnv } from '@dehub/shared/config';
+import { defaultEnv, Env } from './env';
+
+export const environment: Env = {
+  ...defaultEnv,
+  ...defaultSharedPreviewEnv,
+
+  baseUrl: '/buy',
+
+  swap: {
+    defaultInput: 'BNB',
+    defaultOutput: '0xFC206f429d55c71cb7294EfF40c6ADb20dC21508',
+    factory: '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73',
+    initCodeHash:
+      '0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5',
+    router: '0x10ED43C718714eb63d5aA57B78B54704E256024E',
+    slippage: 1400,
+  },
+};
