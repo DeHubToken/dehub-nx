18cd15d156898bbdd674714db3e7dbbd3d89280e
Aleksandr Pasevin <pasevin@gmail.com>
2022-05-27 10:19:13 +0300

    fix(web): lint issues

--- libs/angular/ui/src/lib/pipes/as/as.pipe.ts
+++ libs/angular/ui/src/lib/pipes/as/as.pipe.ts
@@ -2,6 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'as', pure: true })
 export class AsPipe implements PipeTransform {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   transform<S, T extends S>(value: S, type?: new () => T): T {
     return <T>value;
   }
