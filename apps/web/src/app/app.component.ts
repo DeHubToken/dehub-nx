e445e399d2ee28097be0813f5746396c60430635
Aleksandr Pasevin <pasevin@gmail.com>
2022-05-31 10:26:40 +0300

    feat(web): feature-auth with routing, guards, and next support

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { CoreService, PwaService } from '@dehub/angular/core';
+import { CoreService, LoaderService, PwaService } from '@dehub/angular/core';
 import { MenuMode, ThemeMode } from '@dehub/shared/model';
 import { PrimeNGConfig } from 'primeng/api';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'dhb-root',
   template: `
@@ -12,6 +13,13 @@ import { PrimeNGConfig } from 'primeng/api';
       (update)="onSwUpdate()"
       (cancel)="onSwCancel()"
     ></dhb-sw-update-available>
+
+    <!-- Loader -->
+    <dhb-loader
+      *ngIf="(loaderVisible$ | async)!"
+      [subtitle]="(subtitle$ | async)!"
+      [lottieJson]="lottieJson"
+    ></dhb-loader>
   `,
 })
 export class AppComponent implements OnInit, OnDestroy {
@@ -21,10 +29,15 @@ export class AppComponent implements OnInit, OnDestroy {
   topbarTheme: ThemeMode = 'dark';
   menuTheme: ThemeMode = 'dark';
 
+  loaderVisible$?: Observable<boolean>;
+  subtitle$?: Observable<string>;
+  lottieJson?: string;
+
   constructor(
     private coreService: CoreService,
     private pwaService: PwaService,
-    private primengConfig: PrimeNGConfig
+    private primengConfig: PrimeNGConfig,
+    private loaderService: LoaderService
   ) {}
 
   ngOnInit() {
@@ -32,6 +45,11 @@ export class AppComponent implements OnInit, OnDestroy {
     this.coreService.loadIcon();
 
     this.pwaService.subscribeForNewUpdates();
+
+    const { loaderVisible$, subtitle$, lottieJson } = this.loaderService;
+    this.loaderVisible$ = loaderVisible$;
+    this.subtitle$ = subtitle$;
+    this.lottieJson = lottieJson;
 
     this.primengConfig.ripple = true;
   }
---
+++ libs/angular/core/src/lib/guards/authenticated.guard.ts
@@ -0,0 +1,30 @@
+import { Inject, Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { IMoralisService, MoralisToken } from '@dehub/angular/model';
+import { map } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticatedGuard implements CanActivate {
+  constructor(
+    @Inject(MoralisToken) private moralisService: IMoralisService,
+    private router: Router
+  ) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.moralisService.isAuthenticated$.pipe(
+      map(isAuthenticated => (isAuthenticated ? true : this.redirect(state)))
+    );
+  }
+
+  private redirect(state: RouterStateSnapshot) {
+    this.router.navigate(['/', { outlets: { modal: ['auth'] } }], {
+      queryParams: { nextUrl: state.url },
+    });
+    return false;
+  }
+}
