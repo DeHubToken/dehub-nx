dba0a0b112ed5ca15220850f68e482f1f5360cf2
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2022-06-02 11:15:22 +0200

    fix: p-inplace need Toast module, not sure why so far

--- libs/angular/feature-auth/src/lib/angular-feature-auth.module.ts
+++ libs/angular/feature-auth/src/lib/angular-feature-auth.module.ts
@@ -6,6 +6,7 @@ import { LoaderModule } from '@dehub/angular/ui/components/loader';
 import { WalletButtonModule } from '@dehub/angular/ui/components/wallet-button';
 import { InplaceModule } from 'primeng/inplace';
 import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
 import { AngularFeatureAuthRoutingModule } from './angular-feature-auth-routing.module';
 import { AngularFeatureAuthComponent } from './angular-feature-auth.component';
 import {
@@ -28,6 +29,7 @@ export const angularFeatureAuthRoutes: Route[] = [];
     // PrimeNg
     InplaceModule,
     InputTextModule,
+    ToastModule, // TODO: not sure why p-inplace need this
 
     // Libs
     WalletButtonModule,
