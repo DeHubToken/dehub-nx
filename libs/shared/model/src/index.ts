9255cecba3a5bac220b3d408f44dbc217398b585
Aleksandr Pasevin <pasevin@gmail.com>
2022-06-06 16:14:31 +0300

    feat(shared): shop model with request params and responses

--- libs/shared/model/src/index.ts
+++ libs/shared/model/src/index.ts
@@ -4,5 +4,6 @@ export * from './lib/freya.model';
 export * from './lib/location.model';
 export * from './lib/moralis.model';
 export * from './lib/prime.model';
+export * from './lib/shop.model';
 export * from './lib/swiper.model';
 export * from './lib/website-contentful.model';
---
+++ libs/shared/model/src/lib/shop.model.ts
@@ -0,0 +1,30 @@
+import { PhysicalAddress } from './location.model';
+
+/**
+ * A set of product values required for Order initialization on Moralis.
+ */
+export interface ProductData {
+  name: string;
+  picture: string;
+  sku: string;
+}
+
+/**
+ * Moralis Order initialization parameters.
+ * 'productData' and 'shippingAddress' can be parsed objects used in frontend.
+ * Or a stringified object if received via API request by the cloud function.
+ */
+export interface InitOrderParams {
+  address: string;
+  productData: ProductData | string;
+  shippingAddress: PhysicalAddress | string;
+  contentfulId: string;
+}
+
+/**
+ * Moralis Order initialization response.
+ */
+export interface InitOrderResponse {
+  ipfsHash: string;
+  orderId: string;
+}
