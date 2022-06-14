3672c4106dca3b809a0e5d03f820af2cf8310a80
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2022-06-14 09:43:56 +0200

    refactor: remove prediction, prediction-e2e and pcsuikit projects

--- libs/react/pcsuikit/src/lib/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.svg' {
-  const content: string;
-  export default content;
-}
-
-export interface IENavigator extends Navigator {
-  /** IE support */
-  msMaxTouchPoints: number;
-}
