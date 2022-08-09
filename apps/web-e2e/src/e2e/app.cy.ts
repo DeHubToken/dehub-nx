dc3ce8b437d8d9f6c5ec98d9468e0e8838d0d8f9
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2022-08-09 12:33:45 +0200

    test: simple e2e test verification checking DeHub image logo alt

--- apps/web-e2e/src/e2e/app.cy.ts
+++ apps/web-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,9 @@
-import { getGreeting } from '../support/app.po';
+import { getLogoImg } from '../support/app.po';
 
 describe('web', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to web!');
+  it('should have image with alt contains DeHub', () => {
+    getLogoImg().should('have.attr', 'alt').should('include', 'DeHub');
   });
 });
