1a8499580d8be34921e0e51f3fe15271d3156424
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2022-09-27 14:11:25 +0200

    feat(shop): optional referral address during checkout (fix #322) (#327)
    * fix: cloud function d.ts
    
    * feat: add simple referral form and save into moralis db
    
    * build: test will exit on first error
    
    * feat: optional referral address with input mask (42 long)
    
    * style: referral address same structure as contacts

--- apps/moralis/cloud/src/app/shop/shop-functions.e2e.spec.ts
+++ apps/moralis/cloud/src/app/shop/shop-functions.e2e.spec.ts
@@ -1,5 +1,7 @@
 require('../shared/mock.include');
 import {
+  InitOrderParams,
+  InitOrderResult,
   MoralisClass,
   MoralisFunctions,
   OrderStatus,
@@ -27,28 +29,34 @@ describe('E2E Shop functions', () => {
   });
 
   it('Should add new order', async () => {
-    const res = await Moralis.Cloud.run(MoralisFunctions.Shop.InitOrder, {
-      address: config.address,
-      productData: {
-        image:
-          'https://moralis.io/wp-content/uploads/2021/06/Moralis-Glass-Favicon.svg',
-        name: 'name',
-        sku: 'sku',
-      },
-      shippingAddress: {
-        city: 'San Francisco',
-        state: 'California',
-        country: 'US',
-        postalCode: '94116',
-        line1: '2079 46th ave',
-        line2: 'line2',
-        name: 'Ben Weider',
-      },
-      contentfulId: 'contentfulId',
-      quantity: 1,
-      totalAmount: 100,
-      currency: 'BUSD',
-    });
+    const address = config.address;
+    const referralAddress = '0x91573f05f34aaf59ec4849860e61c3762906978e';
+    const res: InitOrderResult | null = await Moralis.Cloud.run(
+      MoralisFunctions.Shop.InitOrder,
+      {
+        address,
+        referralAddress,
+        productData: {
+          image:
+            'https://moralis.io/wp-content/uploads/2021/06/Moralis-Glass-Favicon.svg',
+          name: 'name',
+          sku: 'sku',
+        },
+        shippingAddress: {
+          city: 'San Francisco',
+          state: 'California',
+          country: 'US',
+          postalCode: '94116',
+          line1: '2079 46th ave',
+          line2: 'line2',
+          name: 'Ben Weider',
+        },
+        contentfulId: 'contentfulId',
+        quantity: 1,
+        totalAmount: 100,
+        currency: 'BUSD',
+      } as InitOrderParams
+    );
     expect(res).not.toBeNull;
     expect(res.ipfsHash).toBeDefined;
     expect(res.orderId).toBeDefined;
@@ -59,6 +67,7 @@ describe('E2E Shop functions', () => {
     queryOrders.equalTo('objectId', res.orderId);
     const order = await queryOrders.first();
     expect(order).toBeDefined;
+    expect(order.attributes['referralAddress']).toEqual(referralAddress);
 
     // Check if ShippingAddress is added successfully
     const shippingAddressAbstract = order.get('shippingAddress');
@@ -77,28 +86,31 @@ describe('E2E Shop functions', () => {
   });
 
   it('Should check order status', async () => {
-    const resOrder = await Moralis.Cloud.run(MoralisFunctions.Shop.InitOrder, {
-      address: config.address,
-      productData: {
-        image:
-          'https://moralis.io/wp-content/uploads/2021/06/Moralis-Glass-Favicon.svg',
-        name: 'name',
-        sku: 'sku',
-      },
-      shippingAddress: {
-        city: 'San Francisco',
-        state: 'California',
-        country: 'US',
-        postalCode: '94116',
-        line1: '2079 46th ave',
-        line2: 'line2',
-        name: 'Ben Weider',
-      },
-      contentfulId: 'contentfulId',
-      quantity: 1,
-      totalAmount: 100,
-      currency: 'BUSD',
-    });
+    const resOrder: InitOrderResult | null = await Moralis.Cloud.run(
+      MoralisFunctions.Shop.InitOrder,
+      {
+        address: config.address,
+        productData: {
+          image:
+            'https://moralis.io/wp-content/uploads/2021/06/Moralis-Glass-Favicon.svg',
+          name: 'name',
+          sku: 'sku',
+        },
+        shippingAddress: {
+          city: 'San Francisco',
+          state: 'California',
+          country: 'US',
+          postalCode: '94116',
+          line1: '2079 46th ave',
+          line2: 'line2',
+          name: 'Ben Weider',
+        },
+        contentfulId: 'contentfulId',
+        quantity: 1,
+        totalAmount: 100,
+        currency: 'BUSD',
+      } as InitOrderParams
+    );
     expect(resOrder).not.toBeNull;
     expect(resOrder.ipfsHash).toBeDefined;
     expect(resOrder.orderId).toBeDefined;
