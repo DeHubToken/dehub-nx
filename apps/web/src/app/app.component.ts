b28e2112497a532c65393e4ba93155b84c3cd2d0
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2022-10-07 09:20:47 +0200

    refactor: define announcement component

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,55 +1,10 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  OnDestroy,
-  OnInit,
-} from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CoreService, LoaderService, PwaService } from '@dehub/angular/core';
+import { AnnouncementComponent } from '@dehub/angular/ui/components/announcement';
 import { MenuMode, ThemeMode } from '@dehub/shared/model';
 import { PrimeNGConfig } from 'primeng/api';
 import { DialogService } from 'primeng/dynamicdialog';
 import { Observable } from 'rxjs';
-
-@Component({
-  selector: 'dhb-info',
-  template: `
-    <div class="mt-5 mb-6 text-center">
-      <i class="fa-duotone fa-info icon-color-duotone-1 text-6xl mt-4"></i>
-      <h1>Important update</h1>
-      <h3>
-        Please refer to our
-        <a href="https://t.me/Dehubannouncements/113">official announcement</a>
-      </h3>
-      <h5>TLDR;</h5>
-      <p>
-        We're upgrading the contract,
-        <b>V3 comes with new tokenomics and everything else remains the same</b
-        >.
-      </p>
-      <div>
-        Private Liquidity generation event sold out in 2 days
-        <i
-          >(LGE is not a presale as all funds go into liquidity pools for
-          traders)</i
-        >.
-      </div>
-      <p>
-        Open to all holders with <b>referral system</b> live on our marketplace
-        giving you the
-        <b>chance to earn a 10% bonus on purchases from referral</b>.
-      </p>
-
-      <p>
-        Don't miss the public LGE on the 14th and refer to our announcement page
-        starting at Announcement number 113
-      </p>
-    </div>
-  `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class InfoComponent implements OnInit {
-  ngOnInit() {}
-}
 
 @Component({
   selector: 'dhb-root',
@@ -110,9 +65,9 @@ export class AppComponent implements OnInit, OnDestroy {
 
     setTimeout(
       () =>
-        this.dialogService.open(InfoComponent, {
+        this.dialogService.open(AnnouncementComponent, {
           showHeader: true,
-          header: 'Information',
+          header: 'Announcement',
           width: '620px',
           styleClass: 'bg-gradient-3 border-neon-1',
           closeOnEscape: true,
