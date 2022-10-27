57381d221f5ffacd787ed0c1a62442c46c3ea7d2
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2022-10-27 17:38:15 +0200

    fix(web): announcements not scrollable (#350)
    * fix(web): announcements not scrollable
    
    * style: add extra margin to grab announcement

--- libs/angular/ui/src/lib/components/announcement/announcement.component.ts
+++ libs/angular/ui/src/lib/components/announcement/announcement.component.ts
@@ -5,42 +5,47 @@ import { DynamicDialogConfig } from 'primeng/dynamicdialog';
 @Component({
   selector: 'dhb-announcement',
   template: `
-    <swiper
-      [navigation]="false"
-      [slidesPerView]="'auto'"
-      [direction]="'vertical'"
-    >
-      <ng-container *ngFor="let announcement of announcements">
-        <ng-template swiperSlide>
-          <div [dhbContentfulDraft]="announcement.sys" class="mt-5 mb-6">
-            <!-- Header -->
-            <div
-              class="flex flex-row align-items-center justify-content-center gap-3"
-            >
-              <span class="text-xl text-bold">{{
-                announcement.start | date
-              }}</span>
-              <i
-                [ngClass]="
-                  (announcement.icon || 'fa-megaphone') +
-                  ' fa-duotone icon-color-duotone-1 text-6xl'
-                "
-              ></i>
-            </div>
-            <h1 class="text-center">{{ announcement.header }}</h1>
+    <div class="mx-4">
+      <swiper
+        [navigation]="false"
+        [slidesPerView]="'auto'"
+        [direction]="'vertical'"
+        [freeMode]="true"
+        [scrollbar]="true"
+        [mousewheel]="true"
+      >
+        <ng-container *ngFor="let announcement of announcements">
+          <ng-template swiperSlide>
+            <div [dhbContentfulDraft]="announcement.sys" class="mt-5 mb-6">
+              <!-- Header -->
+              <div
+                class="flex flex-row align-items-center justify-content-center gap-3"
+              >
+                <span class="text-xl text-bold">{{
+                  announcement.start | date
+                }}</span>
+                <i
+                  [ngClass]="
+                    (announcement.icon || 'fa-megaphone') +
+                    ' fa-duotone icon-color-duotone-1 text-6xl'
+                  "
+                ></i>
+              </div>
+              <h1 class="text-center">{{ announcement.header }}</h1>
 
-            <!-- Content -->
-            <div
-              [innerHtml]="
-                announcement.content?.json
-                  | dhbContentfulRichMarkup
-                  | dhbSafeHtml
-              "
-            ></div>
-          </div>
-        </ng-template>
-      </ng-container>
-    </swiper>
+              <!-- Content -->
+              <div
+                [innerHtml]="
+                  announcement.content?.json
+                    | dhbContentfulRichMarkup
+                    | dhbSafeHtml
+                "
+              ></div>
+            </div>
+          </ng-template>
+        </ng-container>
+      </swiper>
+    </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
