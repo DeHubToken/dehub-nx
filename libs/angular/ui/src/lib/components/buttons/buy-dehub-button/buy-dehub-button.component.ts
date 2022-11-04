8d45f2bdc68370f2b1a4272b1f347026ef9cd996
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2022-11-04 17:36:05 +0100

    feat(web): buy dehub redirects to marketplace (#354)

--- libs/angular/ui/src/lib/components/buttons/buy-dehub-button/buy-dehub-button.component.ts
+++ libs/angular/ui/src/lib/components/buttons/buy-dehub-button/buy-dehub-button.component.ts
@@ -7,16 +7,19 @@ import {
   OnInit,
   Output,
 } from '@angular/core';
+import { Router } from '@angular/router';
 import { WINDOW } from '@ng-web-apis/common';
 import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'dhb-buy-dehub-button',
-  template: `<p-splitButton
+  template: ` <!-- https://github.com/DeHubToken/dehub-nx/issues/353 -->
+    <!-- <p-splitButton
     [label]="label"
     [model]="items"
     (onClick)="onBuyClicked($event)"
-  ></p-splitButton>`,
+  ></p-splitButton> -->
+    <p-button [label]="label" (onClick)="onBuyClicked()"></p-button>`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BuyDehubButtonComponent implements OnInit {
@@ -29,7 +32,10 @@ export class BuyDehubButtonComponent implements OnInit {
 
   items: MenuItem[] = [];
 
-  constructor(@Inject(WINDOW) private readonly windowRef: Window) {}
+  constructor(
+    @Inject(WINDOW) private readonly windowRef: Window,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.items = [
@@ -50,8 +56,12 @@ export class BuyDehubButtonComponent implements OnInit {
     ];
   }
 
-  onBuyClicked(event: Event) {
-    event.preventDefault();
-    this.windowRef.open(this.buyDappUrl, '_self', 'noopener,noreferrer');
+  // onBuyClicked(event: Event) {
+  //   event.preventDefault();
+  //   this.windowRef.open(this.buyDappUrl, '_self', 'noopener,noreferrer');
+  // }
+
+  onBuyClicked() {
+    this.router.navigate(['/shop']);
   }
 }
