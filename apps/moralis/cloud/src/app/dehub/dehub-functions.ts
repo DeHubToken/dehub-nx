6009e59256fa9c57f2965d9bb3f05872d2591498
deep-quality-dev <hungrywarrior081@gmail.com>
2023-02-08 08:35:13 -0500

    fix(moralis): rename `totalCirculatingSupply` to `totalSupply`

--- apps/moralis/cloud/src/app/dehub/dehub-functions.ts
+++ apps/moralis/cloud/src/app/dehub/dehub-functions.ts
@@ -20,7 +20,7 @@ export const totalCirculatingSupplyFn = async () => {
   const data = await Moralis.Web3API.native.runContractFunction({
     chain: 'bsc',
     address: '0xFC206f429d55c71cb7294EfF40c6ADb20dC21508',
-    function_name: MoralisFunctions.Dehub.TotalCirculatingSupply,
+    function_name: 'totalSupply',
     // Moralis TS Issue: https://forum.moralis.io/t/react-runcontractfunction-error-codes/4228/25
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore:next-line
