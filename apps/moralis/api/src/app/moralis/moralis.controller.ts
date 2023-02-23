f90d1d9b1bead5026d9daa19b17673e008462e4a
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2023-02-23 13:16:20 +0100

    fix: sample native balance moralis endpoint

--- apps/moralis/api/src/app/moralis/moralis.controller.ts
+++ apps/moralis/api/src/app/moralis/moralis.controller.ts
@@ -2,16 +2,19 @@ import { EvmAddress, EvmChain } from '@moralisweb3/common-evm-utils';
 import { NextFunction, Request, Response } from 'express';
 import Moralis from 'moralis';
 
+/**
+ * Get Native balance
+ * Docs: https://docs.moralis.io/web3-data-api/integrations/aws-lambda-nodejs#create-getnativebalance-endpoint
+ */
 export const nativeBalance = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    // Get native balance
     const nativeBalance = await Moralis.EvmApi.balance.getNativeBalance({
       address: EvmAddress.create(`${req.query.address}`).lowercase,
-      chain: EvmChain.ETHEREUM.name,
+      chain: EvmChain.ETHEREUM.hex,
     });
 
     // Format the native balance formatted in ether via the .ether getter
