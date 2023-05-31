ce9ca6b74588a1ea008d77637b9fee1a65826d0e
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2023-05-31 10:46:25 +0200

    feat(web): new flip card ui component

---
+++ libs/angular/ui/src/lib/components/flip-card/flip-card.component.ts
@@ -0,0 +1,35 @@
+import { NgTemplateOutlet } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+/**
+ * Courtesy: https://www.w3schools.com/howto/howto_css_flip_card.asp
+ */
+@Component({
+  selector: 'dhb-flip-card',
+  standalone: true,
+  imports: [
+    // Angular
+    NgTemplateOutlet,
+  ],
+  template: `
+    <div
+      (click)="flipped = !flipped"
+      [class.flipped]="flipped"
+      class="flip-card animated"
+    >
+      <div class="flip-card-inner">
+        <div class="flip-card-front">
+          <ng-content select="[flip-card-front]" />
+        </div>
+        <div class="flip-card-back">
+          <ng-content select="[flip-card-back]" />
+        </div>
+      </div>
+    </div>
+  `,
+  styleUrls: ['./flip-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FlipCardComponent {
+  flipped = false;
+}
