0b2cfa8c356137d4b766026b1cf714ba5264d30d
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2023-07-19 17:46:41 +0200

    chore: add recommended wallet connect wallet names

--- libs/shared/util/src/lib/moralis.util.ts
+++ libs/shared/util/src/lib/moralis.util.ts
@@ -27,8 +27,11 @@ export const resolveMessage = (state: WalletConnectingState) => {
 export const getWalletConnectQrModalOptions = (legalPage: string) => ({
   themeMode: 'dark',
   explorerRecommendedWalletIds: [
+    // Metamask
     'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
+    // Trust Wallet
     '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
+    // SafePal
     '0b415a746fb9ee99cce155c2ceca0c6f6061b1dbca2d722b3ba16381d0562150',
   ],
   explorerExcludedWalletIds: 'ALL',
