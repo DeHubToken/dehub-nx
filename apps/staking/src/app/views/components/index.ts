6b96aa9869a2d8f40d175a7f00eb86b61df17187
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2023-12-21 09:22:27 +0100

    feat(staking): hide restake logic (#473)

--- apps/staking/src/app/views/components/index.ts
+++ apps/staking/src/app/views/components/index.ts
@@ -1,3 +1,3 @@
-export { default as RestakeModal } from './RestakeModal';
+// export { default as RestakeModal } from './RestakeModal';
 export { default as StakeModal } from './StakeModal';
 export { default as UnstakeModal } from './UnstakeModal';
